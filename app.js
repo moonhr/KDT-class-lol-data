12ec3178c8a4023f6c7edd12545b0d843fb6e26d
moonhr <moonhr153@gmail.com>
2024-05-29 16:44:00 +0900

    qs메서드 __dirname 추가

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const http = require("http");
 const fs = require("fs");
+const path = require("path");
+const qs = require("querystring");
 const memberNames = require("./module/name.js");
 const chamName = require("./module/champion.json")
 
-
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 /**
  * * 서버실행
@@ -100,5 +103,29 @@ for(let data in chamName){
   else{
     res.statusCode = 204;
     res.end();
+  }
+}
+
+function ReadFiles(req, res){
+  //* Resource 같은 경우 대용량 파일이 대부분이다.
+  //* createReadStream을 이용하여 파일을 읽는다.
+  if(req.url.startsWith("/resources/")){
+    const reqEtag = req.headers['if-none-match'];
+    //* 버전에 맞는 이미지 파일인지 확인
+    if(reqEtag != undefined && reqEtag === process.env.RIOT_DATA_VERSION){
+      res.writeHead(304);
+      res.end();
+    }
+    else{
+      res.setHeader('ETag', process.env.RIOT_DATA_VERSION);
+      res.setHeader('Content-Type', GetContentType(req.url));
+      //* 캐시를 설정하여 해당 Resouce를 저장한다.
+      res.setHeader('Cache-Control', 'public, no-transform, max-age=15552000');
+      ReadResouceFile(req.url, res);
+    }
+  }
+  //* 일반 문서 파일들
+  else{
+    SelectFile(res, req.url, GetContentType(req.url));
   }
 }
