5089f79722488e2598a71c688b765b315282ee5c
moonhr <moonhr153@gmail.com>
2024-05-29 23:39:01 +0900

    이벤트 주체 수정

--- public/main.js
+++ public/main.js
@@ -1,13 +1,14 @@
 
+const nameMenu = document.getElementById('name.menu')
+const lineMenu = document.getElementById('line.menu')
+const championMenu = document.getElementById('champion.menu')
+const menus = [
+  nameMenu, lineMenu, championMenu
+]
 
-
-const  nameMenu = document.getElementById('name.menu')
-const  lineMenu = document.getElementById('line.menu')
-const  championMenu = document.getElementById('champion.menu')
-
-nameMenu.addEventListener('input',sendReq)
-lineMenu.addEventListener('input',sendReq)
-championMenu.addEventListener('input',sendReq)
+document.getElementById('name').addEventListener('input',sendReq);
+document.getElementById('line').addEventListener('input',sendReq);
+document.getElementById('cham').addEventListener('input',sendReq);
 
 /**
  * * POST서버로 요청을 보내는 함수
@@ -18,6 +19,9 @@ async function sendReq(){
   //http 요청 생성
   await fetch("http://localhost:8080", {
     method: "POST",
+    headers: {
+      'Content-Type': 'application/json'
+    },
     //body에 json 문자열 반환
     body: JSON.stringify({
       id : elem.id,
@@ -26,13 +30,30 @@ async function sendReq(){
   })
   .then(res => {
     if(res.status == 200){
-      menuOn(elem.id, true);
+      menuOn(true);
     } else{
-      menuOn(elem.id, false);
+      menuOn(false);
     }
   })  
+  .catch(err => {
+    console.error("요청 중 오류 발생:", err);
+    menuOn(false);
+  });
 }
 
-function menuOn(){
-
+/**
+ * * 응답 시 반응 class 추가 삭제
+ * @param {boolean} isTurnOn 
+ */
+function menuOn(isTurnOn){
+  for(let menu of menus){
+    if(isTurnOn){
+      menu.classList.add('menu100');
+      menu.classList.remove('menu20');
+    }
+    else{
+      menu.classList.add('menu20');
+      menu.classList.remove('menu100');
+    }
+  }
 }
