938c01da74e4f29e63dbc5f824936cc591954ebe
moonhr <moonhr153@gmail.com>
2024-05-29 17:26:18 +0900

    server구현 수정

--- app.js
+++ app.js
@@ -5,36 +5,50 @@ const qs = require("querystring");
 const memberNames = require("./module/name.js");
 const chamName = require("./module/champion.json")
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
-/**
- * * 서버실행
- */
-http.createServer((req,res) => {
-  //* GET METHOD
-  if(req.method === "GET"){
-    getMethod(req,res);
+  //*서버 생성
+  const server = http.createServer((req, res) => {
+    let url = req.url;
+    if (req.url === "/favicon.ico") return;
+    //*filePath라는 변수를 getFilePath에 req.url을 매개변수로 삽입한 값으로 할당
+    let filePath = fileUtils.getFilePath(url);
+    //*ext 변수는 getFileExtenstion에 filePath를 삽입한 값으로 할당
+    let ext = fileUtils.getFileExtension(filePath);
+    //*contentType 변수는 getContentType에 ext를 삽입한 값으로 할당
+    let contentType = fileUtils.getContentType(ext);
+    if(req.method === "GET"){
+      getMethod(req, res, filePath, contentType);
+    }
+    //* POST METHOD
+    if(req.method === "POST"){
+      postMethod(req,res);
+    }
+  })
+// 서버를 포트 8080에서 시작
+const port = 8080;
+server.listen(port, (err) => {
+  if (err) {
+    console.log("오류:", err);
+  } else {
+    console.log(`Server is running on port ${port}`);
   }
-  //* POST METHOD
-  if(req.method === "POST"){
-    postMethod(req,res);
-  }
-}).listen(8080);
+});
+
 
 /**
  * * get메서드
  */
-function getMethod(req, res){
-  
-  //* Main Html
-  if(req.url ==='/'){
-    SelectFile(res, 'index.html', "text/html");
-    return;
-  }
-
-  //* OTHER FILE
-  else ReadFiles(req,res);
+function getMethod(req, res, filePath, contentType) {
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.writeHead(404, { "Content-Type": "text/plain; charset=utf-8" });
+      res.end("404 Not Found: 요청하신 파일을 찾을 수 없습니다.");
+      console.log("오류 발생:", err);
+    } else {
+      res.writeHead(200, { "Content-Type": contentType });
+      res.end(data);
+    }
+  });
 }
 
 /**
@@ -106,26 +120,46 @@ for(let data in chamName){
   }
 }
 
-function ReadFiles(req, res){
-  //* Resource 같은 경우 대용량 파일이 대부분이다.
-  //* createReadStream을 이용하여 파일을 읽는다.
-  if(req.url.startsWith("/resources/")){
-    const reqEtag = req.headers['if-none-match'];
-    //* 버전에 맞는 이미지 파일인지 확인
-    if(reqEtag != undefined && reqEtag === process.env.RIOT_DATA_VERSION){
-      res.writeHead(304);
-      res.end();
-    }
-    else{
-      res.setHeader('ETag', process.env.RIOT_DATA_VERSION);
-      res.setHeader('Content-Type', GetContentType(req.url));
-      //* 캐시를 설정하여 해당 Resouce를 저장한다.
-      res.setHeader('Cache-Control', 'public, no-transform, max-age=15552000');
-      ReadResouceFile(req.url, res);
-    }
-  }
-  //* 일반 문서 파일들
-  else{
-    SelectFile(res, req.url, GetContentType(req.url));
-  }
-}
+
+  //*문서 형식에 따른 표기
+  const mimeType = {
+    ".html": "text/html; charset=utf-8",
+    ".css": "text/css",
+    ".js": "application/javascript",
+    ".json": "application/json",
+    ".ico": "image/x-icon",
+    ".jpg": "image/jpeg",
+    ".jpeg": "image/jpeg",
+    ".png": "image/png",
+    ".gif": "image/gif",
+  };
+
+  //*url에 따른 파일 경로 결정 함수 객체
+  const fileUtils = {
+    //*매개변수 url에 따른 파일 경로 할당
+    getFilePath: function (url) {
+      // URL에 따라 파일 경로를 설정
+      if (url === "/") {
+        return path.join(__dirname, "./public/index.html");
+      } else {
+        return path.join(__dirname, `./public${url}`);
+      }
+    },
+    //*파일 경로에 따른 파일 확장자 가져오기
+    getFileExtension: function (filePath) {
+      //*파일 확장자를 가져오는 명령어
+      let ext = path.extname(filePath);
+      //*파일 확장자 소문자로 변환
+      return ext.toLowerCase();
+    },
+    //*파일 확장자에 따른 표기 반환
+    getContentType: function (ext) {
+      //*mimeType에 ext로 가져온 확장자가 있다면 표기 반환
+      if (mimeType.hasOwnProperty(ext)) {
+        return mimeType[ext];
+      } else {
+        return "text/plain";
+      }
+    },
+  };
+
