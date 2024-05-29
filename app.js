ae9d46ac3e65f6dd5645c662cfe5dc6bae287fea
moonhr <moonhr153@gmail.com>
2024-05-29 16:40:38 +0900

    이름, 챔피언 검사 함수

--- app.js
+++ app.js
@@ -3,22 +3,7 @@ const fs = require("fs");
 const memberNames = require("./module/name.js");
 const chamName = require("./module/champion.json")
 
-//* 챔피언 이름만 담을 빈 배열 만들기
-let chams = [];
-//* json 데이터에서 이름 객체 뽑아서 배열에 밀어넣기
-for(let data in chamName){
-  if(data === "data"){
-    for(let a in chamName[data]){
-      for(let b in chamName[data][a]){
-        if(b === 'name'){
-          // console.log(chamName[data][a][b]);
-          // let cham = [];
-          chams.push(chamName[data][a][b]);
-        }
-      }
-    }
-  }
-}
+
 
 /**
  * * 서버실행
@@ -55,16 +40,65 @@ function getMethod(req, res){
 function postMethod(req, res){
   let body = "";
 
-  function a(){
-    console.log("asdasd");console.log("asdasd");console.log("asdasd");
-  }
+  req.on('data',  (data) => body += data );
+  req.on('end', async () => {
+    
+    let elem =JSON.parse(body);
+    let id = elem.id;
+    let value = elem.value;
 
-  function b(){
-    a();a();a();a();a();a();a();a();a();a();a();a();a();a();a();a();a();
-  }
+    if(id == "name"){
+      NameCheck(value, res);
+    }
 
-  function c(){
-    b();b();b();b();b();b();b();b();b();b();b();b();b();b();b();b();b();
-  }
+    else if(id == "cham"){
+      chamCheck(value, res);
+    }
 
+    res.statusCode = 200;
+    res.end();
+  })  
+
+}
+
+//* 이름 검사 함수
+function NameCheck(value, res){
+  const name = memberNames.find(str => str === value);
+  if(name){
+    res.statusCode = 200;
+    res.end();
+  }
+  else{
+    res.statusCode = 204;
+    res.end();
+  }
+}
+
+//* 챔피언 검사 함수
+function chamCheck(value, res){
+//* 챔피언 이름만 담을 빈 배열 만들기
+let chams = [];
+//* json 데이터에서 이름 객체 뽑아서 배열에 밀어넣기
+for(let data in chamName){
+  if(data === "data"){
+    for(let a in chamName[data]){
+      for(let b in chamName[data][a]){
+        if(b === 'name'){
+          // console.log(chamName[data][a][b]);
+          // let cham = [];
+          chams.push(chamName[data][a][b]);
+        }
+      }
+    }
+  }
+}
+  const cham = chams.find(str => str === value);
+  if(cham){
+    res.statusCode = 200;
+    res.end();
+  }
+  else{
+    res.statusCode = 204;
+    res.end();
+  }
 }
