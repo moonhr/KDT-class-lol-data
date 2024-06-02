f1236ed3d32669ecc6d52a741489f0777829b776
moonhr <moonhr153@gmail.com>
2024-06-02 16:28:05 +0900

    클릭 이벤트 발생 시 delete메서드 요청 보냄

---
+++ public/submit.js
@@ -0,0 +1,28 @@
+const ul = document.getElementsByTagName('ul');
+
+ul.addEventListener('click', () => {
+  let del = confirm("삭제하시겠습니까?")
+  if (del) {
+    sendReq();
+  }
+})
+
+//delete method는 body가 필요 없음
+async function sendReq(id) {
+  await fetch(`http://localhost:8080/delete/${id}`), {
+    method: "DELETE",
+    headers: {
+      'Content-Type': 'application/json'
+    },
+  }
+    .then(res => {
+      if (res.status == 200) {
+        alert("삭제되었습니다.")
+      } else {
+        alert("삭제실패")
+      }
+    })
+    .catch(err => {
+      console.error("요청 중 오류 발생:", err);
+    });
+}
