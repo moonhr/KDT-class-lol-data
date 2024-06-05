2d1bc7d7c4de87cb6a9efb94efe43e3f49447b6f
moonhr <moonhr153@gmail.com>
2024-06-05 16:49:30 +0900

    함수 내보내기

---
+++ module/dataJsonName.js
@@ -0,0 +1,21 @@
+const fs = require('fs')
+
+/**
+ * 배열에 data에 존재하는 json이름들을 넣어주는 함수
+ * @param {빈배열} nameArr 
+ */
+function dataJsonName(nameArr){
+  let dataDir = fs.readdirSync(`./data`, (err) => {
+    if (err) {
+      console.log(err);
+    }
+  });
+  dataDir.forEach(name => {
+    // name = path.basename(dataDir, ".json")
+    name = name.replace(".json","");
+    nameArr.push(name);
+  });
+
+}
+
+module.exports = dataJsonName;
